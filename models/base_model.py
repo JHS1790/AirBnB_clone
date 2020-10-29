dc28a2b7a6261a0e672b381e5f57537c02ca4b69
JHS1790 <1790@holbertonschool.com>
2020-10-29 16:36:54 +0000

    first draft on task 4

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,31 @@ class BaseModel():
 
 	'''
 
-	def __init__(self):
+	def __init__(self, *args, **kwargs):
 		'''initialize Minuteman III, or not hahahaha, unless...?
 
+		Args:
+			*args: Black Magic
+			**kwargs: Even More Black Magic (also you should just pump a dict into this)
 		'''
-		self.id = str(uuid4())
-		self.created_at = datetime.now()	
-		self.updated_at = datetime.now()
+		if 'id' in kwargs:
+			self.id = kwargs.get('id')
+		else:
+			self.id = str(uuid4())
+		if 'created_at' in kwargs:
+			self.created_at = datetime.strptime(kwargs.get('created_at'), '%Y-%m-%dT%H:%M:%S.%f')
+		else:
+			self.created_at = datetime.now()	
+		if 'updated_at' in kwargs:
+			self.updated_at = datetime.strptime(kwargs.get('updated_at'), '%Y-%m-%dT%H:%M:%S.%f')
+		else:
+			self.updated_at = datetime.now()
 
 	def __str__(self):
 		'''The string of fate has been severed, persist in this doomed world you have created
 
+		Return:
+			the completed print_string: [<class name>] (<self.id>) <self.__dict__>
 		'''
 		print_string = "["
 		print_string += str(self.__class__.__name__)
@@ -38,6 +52,8 @@ class BaseModel():
 	def to_dict(self):
 		'''returns a dictionary containing all keys/values of __dict__ of the instance
 
+		Return:
+			new_dict, basically just __dict__ updated with instance class name and datetimes
 		'''
 		new_dict = self.__dict__
 		new_dict['__class__'] = str(self.__class__.__name__)
