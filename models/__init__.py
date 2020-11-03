ce4b5ff41b9d30b8f852bf7f2346776c2edbf10a
JHS1790 <1790@holbertonschool.com>
2020-11-03 15:08:16 +0000

    go at 10

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 '''models module'''
 
-__all__ = ['BaseModel', 'User']
+__all__ = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
 from .storage_enabler import storage
 from .base_model import BaseModel
 from .user import User
+from .state import State
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .review import Review
