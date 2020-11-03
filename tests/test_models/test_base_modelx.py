cbeb4e273922001d37edd7704db8a71ad6e9fd6d
JHS1790 <1790@holbertonschool.com>
2020-11-03 16:10:36 +0000

    Half hearted start to Unittests

---
+++ tests/test_models/test_base_modelx.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''UNITTESTS BABY eml'''
+
+import unittest
+import json
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    '''TestBaseModel: Based'''
+
+    
